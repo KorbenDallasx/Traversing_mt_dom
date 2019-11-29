73777371e5cd51b02633781f62638852ca3c9c5d
Rian Ryan <48928738+KorbenDallasx@users.noreply.github.com>
2019-11-29 13:46:59 +0000

    traversing mt dom 2

---
+++ Traversing_mt_dom/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	//sets <a> element within paragraph  to yellow  
+	$("p").click(function(){
+		$(this).children("a").css("background-color", "yellow"); /* returns all the <a> child elements that are 
+		within this paragraph*/
+	});
+});
